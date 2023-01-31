ca0118d654c5f68ac6a7dc0aff3aa445e9b14003
Isaiah peter <peterisaiah4fun@gmail.com>
2023-01-31 21:10:28 +0100

    task 1

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""lockbox"""
+
+
 def join(T, R):
+    """join the lock"""
     res = []
     for e in R:
         res += T[e]
@@ -7,6 +11,7 @@ def join(T, R):
 
 
 def canUnlockAll(boxes):
+    """check can lock"""
     index = 0
     total = list(set(boxes[0]) | {0})
     added = True
