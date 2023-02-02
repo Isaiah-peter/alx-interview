8745873db842ccb67f01586e58eafdf511b28b98
Isaiah peter <peterisaiah4fun@gmail.com>
2023-02-02 23:11:54 +0100

    task 1

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -2,26 +2,22 @@
 """lockbox"""
 
 
-def join(T, R):
-    """join the lock"""
-    res = []
-    for e in R:
-        res += T[e]
-    return res
-
-
 def canUnlockAll(boxes):
-    """check can lock"""
-    index = 0
-    total = list(set(boxes[0]) | {0})
-    added = True
-    while added:
-        added = False
-        for j in join(boxes, total[index:]):
-            if j not in total:
-                total.append(j)
-                index += 1
-                added = True
-    print(total)
-
-    return len(total) == len(boxes)
+    """
+    Function that checks with boolean value if the list type and
+    length to invoke two for iterations one to traverse the list
+    and the other to compaer if key is idx or not in order to open
+    """
+    if type(boxes) is not list:
+        return False
+    elif (len(boxes)) == 0:
+        return False
+    for k in range(1, len(boxes) - 1):
+        boxes_checked = False
+        for idx in range(len(boxes)):
+            boxes_checked = k in boxes[idx] and k != idx
+            if boxes_checked:
+                break
+        if boxes_checked is False:
+            return boxes_checked
+    return True
