5880e4ae06b40d3600225e3d5f9d47118f492c67
Isaiah peter <peterisaiah4fun@gmail.com>
2023-03-16 17:18:12 +0100

    task 1

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,25 +3,12 @@
 
 
 def rotate_2d_matrix(matrix):
-    N = len(matrix)
-    # Consider all squares one by one
-    for x in range(0, int(N / 2)):
+    for i in range(len(matrix)):
+        for j in range(i, len(matrix)):
 
-        # Consider elements in group
-        # of 4 in current square
-        for y in range(x, N-x-1):
+            # swapping matrix[i][j] and matrix[j][i]
+            matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
-            # store current cell in temp variable
-            temp = matrix[x][y]
-
-            # move values from right to top
-            matrix[x][y] = matrix[y][N-1-x]
-
-            # move values from bottom to right
-            matrix[y][N-1-x] = matrix[N-1-x][N-1-y]
-
-            # move values from left to bottom
-            matrix[N-1-x][N-1-y] = matrix[N-1-y][x]
-
-            # assign temp to left
-            matrix[N-1-y][x] = temp
+    # reverse
+    for i in range(len(matrix)):
+        matrix[i].reverse()
